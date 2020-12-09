524e7423558eb665a9a620b479a03c3973ae9f52
Павлов Иван Иванович <w3dipmail@gmail.com>
2020-12-10 00:52:11 +0300

    coverage 100%

--- client_test.go
+++ client_test.go
@@ -11,19 +11,8 @@ import (
 	"os"
 	"strconv"
 	"testing"
+	"time"
 )
-
-//type TestCase struct {
-//	ID      string
-//	Result  *CheckoutResult
-//	IsError bool
-//}
-//
-//type CheckoutResult struct {
-//	Status  int
-//	Balance int
-//	Err     string
-//}
 
 type Root struct {
 	XMLName xml.Name `xml:"root"`
@@ -53,6 +42,77 @@ type Row struct {
 }
 
 func SearchServer(w http.ResponseWriter, r *http.Request) {
+	token := r.Header.Get("AccessToken")
+	if token == "" {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	limit := r.FormValue("limit")
+
+	query := r.FormValue("query")
+	if query == "fail" {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if query == "wrong_body" {
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, `{"status": 400}`)
+		return
+	}
+
+	offset := r.FormValue("offset")
+	if offset == "100" {
+		w.WriteHeader(http.StatusBadRequest)
+		error := SearchErrorResponse{
+			Error: "UnknownBadRequest",
+		}
+		error_result, err := json.Marshal(error)
+		if err != nil {
+			panic(err)
+		}
+		io.WriteString(w, string(error_result))
+		return
+	}
+
+	if offset == "101" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if offset == "102" {
+		//w.Header().Set("Location", "")
+		//return
+		panic("Server failed")
+	}
+
+	order := r.FormValue("order_field")
+	switch order {
+	case "Id", "Age", "Name":
+		//поиск по полям
+		break
+	case "":
+		//поиск по Name
+		break
+	case "About":
+		time.Sleep(2 * time.Second)
+		w.WriteHeader(http.StatusOK)
+		break
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		error := SearchErrorResponse{
+			Error: "ErrorBadOrderField",
+		}
+		error_result, err := json.Marshal(error)
+		if err != nil {
+			panic(err)
+		}
+		io.WriteString(w, string(error_result))
+		return
+		//panic("Wrong order type")
+	}
+
 	file, err := os.Open("dataset.xml")
 	if err != nil {
 		panic(err)
@@ -75,14 +135,12 @@ func SearchServer(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			// handle error
 			fmt.Println(err)
-			//os.Exit(2)
 		}
 
 		age, err := strconv.Atoi(row.Age)
 		if err != nil {
 			// handle error
 			fmt.Println(err)
-			//os.Exit(2)
 		}
 		users = append(users, User{
 			Id:     id,
@@ -93,100 +151,25 @@ func SearchServer(w http.ResponseWriter, r *http.Request) {
 		})
 	}
 	w.WriteHeader(http.StatusOK)
-	result, err := json.Marshal(users)
+
+	new_users := make([]User, 0)
+	if limit == "21" {
+		for indx := 0; indx < 21; indx++ {
+			new_users = append(new_users, users[indx])
+		}
+	} else {
+		for indx := 0; indx < len(users); indx++ {
+			new_users = append(new_users, users[indx])
+		}
+	}
+	result, err := json.Marshal(new_users)
 	if err != nil {
 		panic(err)
 	}
 	io.WriteString(w, string(result))
-
-	//key := r.FormValue("id")
-	//switch key {
-	//case "42":
-	//	w.WriteHeader(http.StatusOK)
-	//	io.WriteString(w, `{"status": 200, "balance": 100500}`)
-	//case "100500":
-	//	w.WriteHeader(http.StatusOK)
-	//	io.WriteString(w, `{"status": 400, "err": "bad_balance"}`)
-	//case "__broken_json":
-	//	w.WriteHeader(http.StatusOK)
-	//	io.WriteString(w, `{"status": 400`) //broken json
-	//case "__internal_error":
-	//	fallthrough
-	//default:
-	//	w.WriteHeader(http.StatusInternalServerError)
-	//}
 }
 
-//func parseXml() {
-//	logins := make([]string, 0)
-//	v := new(Users)
-//	err := xml.Unmarshal(xmlData, &v)
-//	if err != nil {
-//		fmt.Printf("error: %v", err)
-//		return
-//	}
-//	for _, u := range v.List {
-//		logins = append(logins, u.Login)
-//	}
-//}
-
-//type Cart struct {
-//	PaymentApiURL string
-//}
-//
-//func (c *Cart) Checkout(id string) (*CheckoutResult, error) {
-//	url := c.PaymentApiURL + "?id=" + id
-//	resp, err := http.Get(url)
-//	if err != nil {
-//		return nil, err
-//	}
-//	defer resp.Body.Close()
-//
-//	data, err := ioutil.ReadAll(resp.Body)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	result := &CheckoutResult{}
-//
-//	err = json.Unmarshal(data, result)
-//	if err != nil {
-//		return nil, err
-//	}
-//	return result, nil
-//}
-
-func TestCartCheckout(t *testing.T) {
-	//cases := []TestCase{
-	//	TestCase{
-	//		ID: "42",
-	//		Result: &CheckoutResult{
-	//			Status:  200,
-	//			Balance: 100500,
-	//			Err:     "",
-	//		},
-	//		IsError: false,
-	//	},
-	//	TestCase{
-	//		ID: "100500",
-	//		Result: &CheckoutResult{
-	//			Status:  400,
-	//			Balance: 0,
-	//			Err:     "bad_balance",
-	//		},
-	//		IsError: false,
-	//	},
-	//	TestCase{
-	//		ID:      "__broken_json",
-	//		Result:  nil,
-	//		IsError: true,
-	//	},
-	//	TestCase{
-	//		ID:      "__internal_error",
-	//		Result:  nil,
-	//		IsError: true,
-	//	},
-	//}
+func TestFindUsers(t *testing.T) {
 
 	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
 
@@ -210,21 +193,205 @@ func TestCartCheckout(t *testing.T) {
 	if result == nil {
 		t.Error("Result is empty")
 	}
-	//for caseNum, item := range cases {
-	//	c := &Cart{
-	//		PaymentApiURL: ts.URL,
-	//	}
-	//	result, err := c.Checkout(item.ID)
-	//
-	//	if err != nil && !item.IsError {
-	//		t.Errorf("[%d] unexpected error: %#v", caseNum, err)
-	//	}
-	//	if err == nil && item.IsError {
-	//		t.Errorf("[%d] expected error, got nil", caseNum)
-	//	}
-	//	if !reflect.DeepEqual(item.Result, result) {
-	//		t.Errorf("[%d] wrong result, expected %#v, got %#v", caseNum, item.Result, result)
-	//	}
-	//}
+	ts.Close()
+}
+
+func TestFindUsersLimitLessZero(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	client := &SearchClient{
+		URL:         ts.URL,
+		AccessToken: "some token",
+	}
+
+	req := SearchRequest{
+		Limit:      -1,
+		OrderField: "",
+	}
+	client.FindUsers(req)
+	ts.Close()
+}
+
+func TestFindUsersLimitGreaterMax(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	client := &SearchClient{
+		URL:         ts.URL,
+		AccessToken: "some token",
+	}
+
+	req := SearchRequest{
+		Limit:      26,
+		OrderField: "",
+	}
+	client.FindUsers(req)
+	ts.Close()
+}
+
+func TestFindUsersOffsetLessZero(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	client := &SearchClient{
+		URL:         ts.URL,
+		AccessToken: "some token",
+	}
+
+	req := SearchRequest{
+		Offset:     -1,
+		OrderField: "",
+	}
+	client.FindUsers(req)
+	ts.Close()
+}
+
+func TestFindUsersTimeout(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	client := &SearchClient{
+		URL:         ts.URL,
+		AccessToken: "some token",
+	}
+
+	req := SearchRequest{
+		Offset:     0,
+		OrderField: "About",
+	}
+	client.FindUsers(req)
+	ts.Close()
+}
+
+func TestFindUsersUnauthorized(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	client := &SearchClient{
+		URL:         ts.URL,
+		AccessToken: "",
+	}
+
+	req := SearchRequest{
+		Offset:     0,
+		OrderField: "About",
+	}
+	client.FindUsers(req)
+	ts.Close()
+}
+
+func TestFindUsersServerError(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	client := &SearchClient{
+		URL:         ts.URL,
+		AccessToken: "some toke",
+	}
+
+	req := SearchRequest{
+		Offset: 0,
+		Query:  "fail",
+	}
+	client.FindUsers(req)
+	ts.Close()
+}
+
+func TestFindUsersBadRequestWithCode(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	client := &SearchClient{
+		URL:         ts.URL,
+		AccessToken: "some token",
+	}
+
+	req := SearchRequest{
+		Offset:     0,
+		OrderField: "Gender",
+	}
+	client.FindUsers(req)
+	ts.Close()
+}
+
+func TestFindUsersBadRequestWrongOffset(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	client := &SearchClient{
+		URL:         ts.URL,
+		AccessToken: "some token",
+	}
+
+	req := SearchRequest{
+		Offset:     100,
+		OrderField: "Gender",
+	}
+	client.FindUsers(req)
+	ts.Close()
+}
+
+func TestFindUsersBadRequestCommonCase(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	client := &SearchClient{
+		URL:         ts.URL,
+		AccessToken: "some token",
+	}
+
+	req := SearchRequest{
+		Offset:     101,
+		OrderField: "Gender",
+	}
+	client.FindUsers(req)
+	ts.Close()
+}
+
+func TestFindUsersServerFailed(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	client := &SearchClient{
+		URL:         ts.URL,
+		AccessToken: "some token",
+	}
+
+	req := SearchRequest{
+		Offset:     102,
+		OrderField: "Gender",
+	}
+	client.FindUsers(req)
+	ts.Close()
+}
+
+func TestFindUsersFailedParsing(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	client := &SearchClient{
+		URL:         ts.URL,
+		AccessToken: "some token",
+	}
+
+	req := SearchRequest{
+		Limit:      10,
+		Offset:     0,
+		Query:      "wrong_body",
+		OrderField: "",
+		OrderBy:    0,
+	}
+	client.FindUsers(req)
+	ts.Close()
+}
+
+func TestFindUsersLimitEqualsSize(t *testing.T) {
+
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	client := &SearchClient{
+		URL:         ts.URL,
+		AccessToken: "some token",
+	}
+
+	req := SearchRequest{
+		Limit:      20,
+		Offset:     0,
+		Query:      "Name",
+		OrderField: "",
+		OrderBy:    0,
+	}
+
+	client.FindUsers(req)
 	ts.Close()
 }
